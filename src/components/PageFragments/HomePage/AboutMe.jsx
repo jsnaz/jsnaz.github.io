453c998ae4b8c0ec513425370272b3f6cfa5786b
Yinshangyi <jl.dejaeger@gmail.com>
2021-03-21 04:14:12 +0100

    Fix build issue

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -5,16 +5,13 @@ import { stripTags } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 import { pageText, funFactsData } from './about.data';
 
-const isMobile = window.innerWidth < 758;
-const mobileMultiRatio = 1.2;
-
 const makeParagraph = (text) => text.split('\n').map((line) => <p className="aboutText">{line}</p>);
 const makeFunFacts = (funFacts) => funFacts.map((funFact) => (
   <Fragment key={funFact.id}>
     <Col xs={24} sm={24} md={12} lg={8}>
       <AboutTile
         img={funFact.img}
-        height={isMobile ? funFact.imgSize * mobileMultiRatio : funFact.imgSize}
+        height={funFact.imgSize}
         alt="location image"
         textH4={funFact.topLine}
         textH3={funFact.bottomLine}
